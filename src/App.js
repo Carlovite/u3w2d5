4f902cf9e36350e4cea07d0724293f2519a6b4fc
Carlo Vitetta <vitettacarlo@gmail.com>
2023-11-04 18:04:40 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ import Tenerife from "./Components/Tenerife";
 function App() {
   return (
     <>
-      <Container>
-        <div className="text-center mt-3">
-          <span>WEATHER APP</span>
+      <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+        <div className="text-center text-white">
+          <span>THE WEATHER</span>
         </div>
         <div className="d-flex justify-content-center">
           <div>
             {" "}
-            <p>
+            <p className="text-white">
               Day: {moment().format("dddd")} {moment().format("LL")}
             </p>
           </div>
         </div>
-        <Row className="mt-3 gy-3">
+        <Row className="mt-3 gy-3 gx-5">
           <Milan></Milan>
           <Berlin></Berlin>
           <London></London>
